9a4a880c877a454d3ac07a6bf457ab9a6f075069
m0h1m3d <mohisham33@gmail.com>
2024-03-08 10:20:50 +0000

    add display module and refractor code

--- script.js
+++ script.js
@@ -2,42 +2,6 @@ console.log(
   'game preview:\nchoose number for positioning\nfirst to 3 rounds wins the game!\n\n  0  |  1  |  2  \n-----------------\n  3  |  4  |  5  \n-----------------\n  6  |  7  |  8'
 );
 
-const styles = (() => {
-  function init() {
-    const cpuBtn = document.querySelector('.cpu');
-    const pLayerTwoBtn = document.querySelector('.user');
-
-    cpuBtn.addEventListener('click', () => {
-      cpuBtn.style.boxShadow = '0 1px 0 rgb(25, 56, 56)';
-      cpuBtn.style.transform = 'translateY(9px)';
-      setTimeout(() => {
-        cpuBtn.style.transform = 'translateY(0px)';
-        cpuBtn.style.boxShadow = '0 10px 0 rgb(25, 56,  56)';
-      }, 100);
-    });
-
-    pLayerTwoBtn.addEventListener('click', () => {
-      pLayerTwoBtn.style.boxShadow = '0 1px 0 rgb(114, 86, 35)';
-      pLayerTwoBtn.style.transform = 'translateY(9px)';
-      setTimeout(() => {
-        pLayerTwoBtn.style.transform = 'translateY(0px)';
-        pLayerTwoBtn.style.boxShadow = '0 10px 0 rgb(114, 86, 35)';
-      }, 100);
-    });
-  };
-
-  function toggleHide(els){
-    els.forEach(el => {
-      setTimeout(()=>{
-        el.classList.toggle("hidden");
-      },350);      
-    });
-  };
-
-  return { init, toggleHide };
-})();
-styles.init();
-
 const board = (() => {
   const positions = ['', '', '', '', '', '', '', '', ''];
   let display;
@@ -46,29 +10,32 @@ const board = (() => {
 })();
 const playerOne = (() => {
   const name = 'Player One';
-  const sign = 'X';
+  const sign = '';
   const moves = [];
   const roundWins = 0;
+  const color = '#f7b844';
 
-  return { name, sign, moves, roundWins };
+  return { name, sign, moves, roundWins, color };
 })();
 
 const playerTwo = (() => {
   const name = 'Player Two';
-  const sign = playerOne.sign === 'X' ? 'O' : 'X';
+  const sign = '';
   const moves = [];
   const roundWins = 0;
+  const color = '#33B9B9';
 
-  return { name, sign, moves, roundWins };
+  return { name, sign, moves, roundWins, color };
 })();
 
 const cpu = (() => {
   const name = 'CPU';
-  const sign = playerOne.sign === 'X' ? 'O' : 'X';
+  const sign = '';
   const moves = [];
   const roundWins = 0;
+  const color = '#33B9B9';
 
-  return { name, sign, moves, roundWins };
+  return { name, sign, moves, roundWins, color };
 })();
 
 const game = (() => {
@@ -86,21 +53,33 @@ const game = (() => {
 
   let currentPlayer = playerOne;
   let nextPlayer;
-  const pick = document.querySelector('.pick');
-  const btnsPick = document.querySelector('.btns-pick');
-  
+  const pick = document.querySelector('.pick-container');
+
   pick.addEventListener('click', (e) => {
     if (e.target.className.includes('user')) {
-      styles.toggleHide([pick,btnsPick,boardEl]);      
+      if(playerOne.sign === '') return;
 
       nextPlayer = playerTwo;
       init(currentPlayer, nextPlayer);
+
     } else if (e.target.className.includes('cpu')) {
-      styles.toggleHide([pick,btnsPick,boardEl]);      
-     
-      
+      if(playerOne.sign === '') return;
+
       nextPlayer = cpu;
       init(currentPlayer, nextPlayer);
+
+    }else if(e.target.className.includes('mark-x')){
+
+      playerOne.sign = 'X';
+      playerTwo.sign = 'O';
+      cpu.sign = 'O';
+
+    }else if(e.target.className.includes('mark-o')){
+      
+      playerOne.sign = 'O';
+      playerTwo.sign = 'X';
+      cpu.sign = 'X';
+
     }
   });
 
@@ -110,16 +89,16 @@ const game = (() => {
 
   function playRound(player, nextPlayer) {
     let playerMove;
-    setMove(player, (move) => {
+    renderMove(player, (move) => {
       playerMove = move;
 
-      if (!validMove(playerMove)) {
+      if (!validMove(playerMove)) {        
         console.log('position taken');
         playRound(player, nextPlayer);
         return;
       };
 
-      displayMove(player, playerMove);
+      addMove(player, playerMove);
       getRoundWinner(player, nextPlayer);
       checkGameWinner(player, nextPlayer);
 
@@ -142,41 +121,59 @@ const game = (() => {
     });
   };
 
-  function setMove(player, callback) {
+  function renderMove(player, callback) {
     if (player.name === 'CPU') {
-      let validMoves = board.positions.reduce((acc,position,index)=>{
-        if(position === '') acc.push(index);
-        return acc;
-      },[]);
+     let move = getCpuMove();
+     const cell = document.querySelector(`[data-position = "${move}"]`);
 
-        let randomIndex = Math.floor(Math.random()*validMove.length);
-        let move = validMoves[randomIndex];
-        callback(move);
+     domDisplay.renderMark(cell, player);
+
+      callback(move);
     } else {
       const handleClick = (e) => {
         if (e.target.tagName === 'BUTTON') {
           let move = +e.target.dataset.position;
+
+          if(validMove(move)){
+            domDisplay.renderMark(e.target, player)
+          }else{
+            domDisplay.renderInvalid(e.target);
+          };
+
           boardEl.removeEventListener('click', handleClick);
           callback(move);
         }
       };
       boardEl.addEventListener('click', handleClick);
-    }
+    };
   };
 
-  function validMove(move) {
-    return board.positions[move] === '';
+  function getCpuMove(){
+    let validMoves = board.positions.reduce((acc,position,index)=>{
+      if(position === '') acc.push(index);
+      return acc;
+    },[]);
+
+      let randomIndex = Math.floor(Math.random()*validMove.length);
+      let move = validMoves[randomIndex];
+      return move;
   };
 
-  function displayMove(player, move) {
+  
+  function addMove(player, move) {
     board.positions[move] = player.sign;
     player.moves.push(move);
-
+      
+        
     console.clear();
-
+    
     board.display = `${board.positions[0]}     |     ${board.positions[1]}     |     ${board.positions[2]}\n------------------------\n${board.positions[3]}     |    ${board.positions[4]}      |     ${board.positions[5]}\n------------------------\n${board.positions[6]}     |     ${board.positions[7]}     |     ${board.positions[8]}`;
 
     console.log(board.display);
+  };
+  
+  function validMove(move) {
+    return board.positions[move] === '';
   };
   
   function winCondition(playerMoves) {
@@ -189,13 +186,19 @@ const game = (() => {
     if (winCondition(player.moves)) {
       player.roundWins++;
       
+      domDisplay.renderWinningUi(player, winingMoves);
+
       console.log(
         `${playerOne.name}:${playerOne.roundWins}\n${nextPlayer.name}:${
           nextPlayer.roundWins
         }\n ${player.roundWins < 3 ? 'next rounds begins!' : 'Game over'}`
       );
+
+      domDisplay.resetCells();
     } else if (boardFull()) {
+      domDisplay.renderTie();
       console.log("It's a tie!\n next round begins!");
+      domDisplay.resetCells();
     };
     
   };
@@ -232,10 +235,158 @@ const game = (() => {
 
   function switchPlayers(player, nextPlayer) {
     if (winCondition(player.moves)) {
-      console.log(player.moves);
       return playerOne;
     } else {
       return player === playerOne ? nextPlayer : playerOne;
     }
   };
 })();
+
+const domDisplay= (()=>{
+  const cpuBtn = document.querySelector('.cpu');
+  const pLayerTwoBtn = document.querySelector('.user');
+  const boardEl = document.querySelector('.board');
+  const boardcells = boardEl.querySelectorAll('button');
+  const pick = document.querySelector('.pick-container');
+  const h1 = pick.querySelector('h1');
+  const btnsPickOp = document.querySelector('.btns-pick-op');
+  const btnMarkX = document.querySelector('.mark-x');
+  const btnMarkO = document.querySelector('.mark-o');
+
+  function init(){
+    
+    clickEffect(cpuBtn, 'rgb(25, 56, 56)');
+    clickEffect(pLayerTwoBtn, 'rgb(114, 86, 35)');
+
+    pick.addEventListener('click', (e)=>{
+      if (e.target.className.includes('user') || e.target.className.includes('cpu')) {
+        if(playerOne.sign === ''){
+          notify();
+          return;
+        };
+  
+        hide([pick,btnsPickOp,btnMarkX,btnMarkO,boardEl]);      
+  
+      }else if(e.target.className.includes('mark-x')){
+  
+        e.target.classList.add('highlight');
+        toggleHighLight([btnMarkO]);
+  
+      }else if(e.target.className.includes('mark-o')){
+        
+        e.target.classList.add('highlight');
+        toggleHighLight([btnMarkX]);
+  
+      }
+    });    
+  };
+
+  function clickEffect(el, color){
+    el.addEventListener('click', () => {
+      el.style.boxShadow = `0 1px 0 ${color}`;
+      el.style.transform = 'translateY(9px)';
+      setTimeout(() => {
+        el.style.transform = 'translateY(0px)';
+        el.style.boxShadow = `0 10px 0 ${color}`;
+      }, 100);
+    });
+  };
+
+  function notify(){
+    h1.style.transform = 'rotate(3deg)';
+
+   setTimeout(()=>{
+       h1.style.transform = 'rotate(-3deg)';
+         setTimeout(()=>{
+            h1.style.transform = 'rotate(0deg)';
+       },300);            
+    },300);
+  }
+
+  function hide(els){
+    els.forEach(el => {
+      setTimeout(()=>{
+        el.classList.toggle("hidden");
+      },350);      
+    });
+  };
+
+  function toggleHighLight(els){
+    els.forEach(el => {
+      el.classList.remove('highlight');
+    });
+  };
+  
+  function resetCells(){
+    boardcells.forEach(cell => {
+      setTimeout(()=>{
+        cell.style.opacity = 0;
+      setTimeout(() => {
+          cell.textContent = '';
+          cell.style.opacity = 1;
+          cell.style.background = '';
+          cell.style.transform = '';
+          cell.classList.remove("winning-cell");
+      }, 1000);
+      },1000)
+  });
+  };
+
+  function renderWinningUi(player,winingMoves){
+    winingMoves.forEach(combo=> {
+        wincombo = combo.every(position=> player.moves.includes(position));
+        
+        if(wincombo){
+          combo.forEach((position)=>{
+            const winingCell = document.querySelector(`[data-position= "${position}"]`);
+              
+            winingCell.classList.add("winning-cell");
+              setTimeout(() => {
+                winingCell.style.transform = 'scale(1)';
+              }, 300);
+          });
+        };
+      });
+  };
+
+  function renderTie(){
+    boardcells.forEach(cell => {
+      cell.style.transform = 'rotate(5deg)';
+
+      setTimeout(()=>{
+      cell.style.transform = '';
+      },350);
+    })
+  }
+
+  function renderMark(el, player){
+    if(player.name === 'CPU'){
+      setTimeout(()=>{
+        el.style.color = player.color;
+        el.textContent = player.sign;
+       el.style.transform = 'scale(1.05)';
+       setTimeout(()=>{
+          el.style.transform = '';
+        },200);
+      },500)
+    }else{
+      el.style.color = player.color;
+      el.textContent = player.sign;
+      el.style.transform = 'scale(1.05)';
+      setTimeout(()=>{
+        el.style.transform = '';
+      },200);
+    }
+  };
+  
+  function renderInvalid(el){
+   el.classList.add('invalid');
+   setTimeout(()=>{
+    el.classList.remove('invalid');
+    },300);
+  };
+
+  return { init,resetCells,renderWinningUi,renderInvalid, renderMark,renderTie }
+
+})();
+domDisplay.init();
