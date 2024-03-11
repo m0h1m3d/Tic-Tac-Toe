b23a42455aac2ec54730b8c75fcaa3480bbdc98b
m0h1m3d <mohisham33@gmail.com>
2024-03-11 09:58:19 +0000

    add logic for game stats and restarting round/game plus refractoring

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-console.log(
-  'game preview:\nchoose number for positioning\nfirst to 3 rounds wins the game!\n\n  0  |  1  |  2  \n-----------------\n  3  |  4  |  5  \n-----------------\n  6  |  7  |  8'
-);
-
 const board = (() => {
   const positions = ['', '', '', '', '', '', '', '', ''];
   let display;
 
   return { positions, display };
 })();
+
 const playerOne = (() => {
   const name = 'Player One';
   const sign = '';
@@ -40,6 +37,12 @@ const cpu = (() => {
 
 const game = (() => {
   const boardEl = document.querySelector('.board');
+  const btnRestartRound = document.querySelector('.btn-restart');
+  const btnRestartGame = document.querySelector('.restartGame');
+
+  let round = 1;
+  let tie = 0;
+
   const winingMoves = [
     [0, 1, 2],
     [3, 4, 5],
@@ -50,11 +53,11 @@ const game = (() => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-
   let currentPlayer = playerOne;
   let nextPlayer;
-  const pick = document.querySelector('.pick-container');
+  let handleClick;
 
+  const pick = document.querySelector('.pick-container');
   pick.addEventListener('click', (e) => {
     if (e.target.className.includes('user')) {
       if(playerOne.sign === '') return;
@@ -83,17 +86,26 @@ const game = (() => {
     }
   });
 
+  btnRestartGame.addEventListener('click', ()=>{
+    resetCurrentGame(currentPlayer, nextPlayer);
+  })
+  btnRestartRound.addEventListener('click', ()=>{
+    resetCurrentGame(currentPlayer, nextPlayer);
+  })
+
   function init(currentPlayer, nextPlayer) {
     playRound(currentPlayer, nextPlayer);
   };
 
   function playRound(player, nextPlayer) {
+    domDisplay.switchTurns(player);
+    domDisplay.setScore(nextPlayer);
+
     let playerMove;
     renderMove(player, (move) => {
       playerMove = move;
 
       if (!validMove(playerMove)) {        
-        console.log('position taken');
         playRound(player, nextPlayer);
         return;
       };
@@ -102,13 +114,11 @@ const game = (() => {
       getRoundWinner(player, nextPlayer);
       checkGameWinner(player, nextPlayer);
 
-      if (gameOver(player)) {
-        
-        resetGame(player, nextPlayer);
-
-      } else if (winCondition(player.moves) || boardFull()) {
+      if (winCondition(player.moves) || boardFull()) {
 
         resetBoard(player, nextPlayer);
+        round++;
+        domDisplay.renderRounds(round);
         currentPlayer = playerOne;
         playRound(currentPlayer, nextPlayer);
     
@@ -122,31 +132,33 @@ const game = (() => {
   };
 
   function renderMove(player, callback) {
-    if (player.name === 'CPU') {
-     let move = getCpuMove();
-     const cell = document.querySelector(`[data-position = "${move}"]`);
+  if (player.name === 'CPU') {
+    let move = getCpuMove();
+    const cell = document.querySelector(`[data-position = "${move}"]`);
 
-     domDisplay.renderMark(cell, player);
-
+    domDisplay.renderMark(cell, player);
+    setTimeout((player)=>{
       callback(move);
-    } else {
-      const handleClick = (e) => {
-        if (e.target.tagName === 'BUTTON') {
-          let move = +e.target.dataset.position;
+      domDisplay.switchTurns(player);
+    }, 500)
+  } else {
+    handleClick = (e) => {
+      if (e.target.tagName === 'BUTTON') {
+        let move = +e.target.dataset.position;
 
-          if(validMove(move)){
-            domDisplay.renderMark(e.target, player)
-          }else{
-            domDisplay.renderInvalid(e.target);
-          };
+        if (validMove(move)) {
+          domDisplay.renderMark(e.target, player)
+        } else {
+          domDisplay.renderInvalid(e.target);
+        };
 
-          boardEl.removeEventListener('click', handleClick);
-          callback(move);
-        }
-      };
-      boardEl.addEventListener('click', handleClick);
+        boardEl.removeEventListener('click', handleClick);
+        callback(move);
+      }
     };
+    boardEl.addEventListener('click', handleClick);
   };
+};
 
   function getCpuMove(){
     let validMoves = board.positions.reduce((acc,position,index)=>{
@@ -163,13 +175,6 @@ const game = (() => {
   function addMove(player, move) {
     board.positions[move] = player.sign;
     player.moves.push(move);
-      
-        
-    console.clear();
-    
-    board.display = `${board.positions[0]}     |     ${board.positions[1]}     |     ${board.positions[2]}\n------------------------\n${board.positions[3]}     |    ${board.positions[4]}      |     ${board.positions[5]}\n------------------------\n${board.positions[6]}     |     ${board.positions[7]}     |     ${board.positions[8]}`;
-
-    console.log(board.display);
   };
   
   function validMove(move) {
@@ -188,26 +193,23 @@ const game = (() => {
       
       domDisplay.renderWinningUi(player, winingMoves);
 
-      console.log(
-        `${playerOne.name}:${playerOne.roundWins}\n${nextPlayer.name}:${
-          nextPlayer.roundWins
-        }\n ${player.roundWins < 3 ? 'next round begins!' : 'Game over'}`
-      );
-
       domDisplay.resetCells();
     } else if (boardFull()) {
+      tie++
+      domDisplay.setTie(tie);
       domDisplay.renderTie();
-      console.log("It's a tie!\n next round begins!");
       domDisplay.resetCells();
     };
     
   };
 
   function checkGameWinner(player, nextPlayer) {
-    if (player.roundWins === 3) {
-      console.log(`${player.name} wins the match!`);
-    } else if (nextPlayer.roundWins === 3) {
-      console.log(`${nextPlayer.name} wins the match!`);
+    if (player.roundWins === 5) {
+      round--;
+      domDisplay.renderGameOver(player);
+    } else if (nextPlayer.roundWins === 5) {
+      round--;
+      domDisplay.renderGameOver(nextPlayer);
     }
   };
   
@@ -221,25 +223,53 @@ const game = (() => {
     nextPlayer.moves = [];
   };
 
-  function gameOver(player) {
-    return player.roundWins === 3;
-  };
-
-  function resetGame(currentPlayer, nextPlayer) {
+  function resetCurrentGame(currentPlayer, nextPlayer) {
     board.positions = ['', '', '', '', '', '', '', '', ''];
     currentPlayer.moves = [];
     currentPlayer.roundWins = 0;
     nextPlayer.moves = [];
     nextPlayer.roundWins = 0;
+    round = 1;
+    tie = 0;
+
+    domDisplay.resetCells();
+    domDisplay.resetScores();
+    switchPlayers(currentPlayer, nextPlayer);
+    domDisplay.switchTurns(playerOne);
+
+  };
+
+  function restartGame(){    
+    boardEl.removeEventListener('click', handleClick);
+
+    board.positions = ['', '', '', '', '', '', '', '', ''];
+    playerOne.moves = [];
+    playerOne.sign = '';
+    playerOne.roundWins = 0;
+    playerTwo.moves = [];
+    playerTwo.sign = '';
+    playerTwo.roundWins = 0;
+    cpu.moves = [];
+    cpu.sign = '';
+    cpu.roundWins = 0;
+    round = 1;
+    tie = 0;
+
+    domDisplay.resetCells();
+    domDisplay.resetScores();
+    domDisplay.switchTurns(playerOne);
   };
 
   function switchPlayers(player, nextPlayer) {
     if (winCondition(player.moves)) {
       return playerOne;
     } else {
+      domDisplay.switchTurns(nextPlayer);
       return player === playerOne ? nextPlayer : playerOne;
     }
   };
+
+  return { btnRestartRound,restartGame }
 })();
 
 const domDisplay= (()=>{
@@ -253,11 +283,38 @@ const domDisplay= (()=>{
   const btnsPickOp = document.querySelector('.btns-pick-op');
   const btnMarkX = document.querySelector('.mark-x');
   const btnMarkO = document.querySelector('.mark-o');
+  const currentTurn = document.querySelector('.current-turn');
+  const opponentName = document.querySelector('.opponent');
+  const opponentScore = document.querySelector('.opScore');
+  const playerName = document.querySelector('.player');
+  const playerScore = document.querySelector('.plScore');
+  const tieScore = document.querySelector('.tScore');
+  const round = document.querySelector('.round');
+  const results = document.querySelector('.results');
+  const btnMenu = document.querySelector('.menu');
+  const btnRestartGame = document.querySelector('.restartGame');
+  const winner = document.querySelector('.winner');
+
+  btnMenu.addEventListener('click', ()=>{
+    game.restartGame();
+    btnMarkO.classList.remove('highlight');
+    btnMarkX.classList.remove('highlight');
+    toggleHide([results,pick,btnsPickOp,btnMarkX,btnMarkO]);
+  });
+
+  btnRestartGame.addEventListener('click', ()=>{
+    toggleHide([results]);
+    toggleHide([gameContainer]);
+  });
 
   function init(){
     
     clickEffect(cpuBtn, 'rgb(25, 56, 56)');
     clickEffect(pLayerTwoBtn, 'rgb(114, 86, 35)');
+    clickEffect(game.btnRestartRound, 'rgb(139, 139, 139)');
+    clickEffect(btnMenu, 'rgb(25, 56, 56)');
+    clickEffect(btnRestartGame, 'rgb(114, 86, 35)');
+
 
     pick.addEventListener('click', (e)=>{
       if (e.target.className.includes('user') || e.target.className.includes('cpu')) {
@@ -266,7 +323,7 @@ const domDisplay= (()=>{
           return;
         };
   
-        hide([pick,btnsPickOp,btnMarkX,btnMarkO,gameContainer]);      
+        toggleHide([pick,btnsPickOp,btnMarkX,btnMarkO,gameContainer]);      
   
       }else if(e.target.className.includes('mark-x')){
   
@@ -288,7 +345,7 @@ const domDisplay= (()=>{
       el.style.transform = 'translateY(9px)';
       setTimeout(() => {
         el.style.transform = 'translateY(0px)';
-        el.style.boxShadow = `0 10px 0 ${color}`;
+        el.style.boxShadow = `0 5px 0 ${color}`;
       }, 100);
     });
   };
@@ -304,7 +361,7 @@ const domDisplay= (()=>{
     },300);
   }
 
-  function hide(els){
+  function toggleHide(els){
     els.forEach(el => {
       setTimeout(()=>{
         el.classList.toggle("hidden");
@@ -324,12 +381,13 @@ const domDisplay= (()=>{
         cell.style.opacity = 0;
       setTimeout(() => {
           cell.textContent = '';
-          cell.style.opacity = 1;
+          cell.style.opacity = '';
+          cell.style.color = '';
           cell.style.background = '';
           cell.style.transform = '';
           cell.classList.remove("winning-cell");
-      }, 1000);
-      },1000)
+      }, 500);
+      },500)
   });
   };
 
@@ -386,7 +444,41 @@ const domDisplay= (()=>{
     },300);
   };
 
-  return { init,resetCells,renderWinningUi,renderInvalid, renderMark,renderTie }
+  function switchTurns(player){
+    currentTurn.textContent = player.sign;
+    currentTurn.style.color = player.color;
+  };
+
+  function renderRounds(rounds){
+    round.textContent = rounds;
+  }
+
+  function setScore(secondPlayer){
+    playerName.textContent = playerOne.name;
+    playerScore.textContent = playerOne.roundWins;
+    opponentName.textContent = secondPlayer.name;
+    opponentScore.textContent = secondPlayer.roundWins;
+  };
+
+  function setTie(tieRounds){
+    tieScore.textContent = tieRounds;
+  };
+
+  function resetScores(){
+    playerScore.textContent = 0;
+    opponentScore.textContent = 0;
+    tieScore.textContent = 0;
+    round.textContent = 1;
+  };
+
+  function renderGameOver(player){
+    toggleHide([gameContainer,results]);
+    winner.textContent = player.name;
+    winner.style.color = player.color;
+    winner.style.fontSize = '2rem';
+  }
+
+  return { init, clickEffect, resetCells,renderWinningUi,renderInvalid, renderMark,renderTie,renderRounds, switchTurns,setScore, setTie, resetScores, renderGameOver }
 
 })();
 domDisplay.init();
